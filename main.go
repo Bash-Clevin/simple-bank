279ab19ec024a95a85aead284cc6ec39e530e613
Bash-Clevin <clevinbash@gmail.com>
2023-11-25 09:16:06 +0300

    feat(db): perform migrations

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"github.com/Bash-clevin/simplebank-go/util"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	runDBMigration(config.MigrationUrl, config.DBSource)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up:", err)
+	}
+
+	log.Println("db migrated successfully")
+}
