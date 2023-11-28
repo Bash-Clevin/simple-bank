fb65d703c89063de460a7abb080e37c64ea8cef6
Bash-Clevin <clevinbash@gmail.com>
2023-11-28 12:17:57 +0300

    feat(db): create user

--- util/random.go
+++ util/random.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"math/rand"
 	"strings"
 	"time"
@@ -45,4 +46,9 @@ func RandomCurrency() string {
 	n := len(currecies)
 
 	return currecies[rand.Intn(n)]
+}
+
+// Generate random email
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
 }
